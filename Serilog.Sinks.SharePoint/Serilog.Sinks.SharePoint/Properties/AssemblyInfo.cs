e608bf6ac3e4f8d906b96d50087d5b8bce103977
valmaev <almaev.vladimir@gmail.com>
2014-08-11 18:48:01 +0400

    Initial solution structure

---
+++ Serilog.Sinks.SharePoint/Serilog.Sinks.SharePoint/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Serilog.Sinks.SharePoint")]
+[assembly: AssemblyDescription("Serilog sink for SharePoint ULS")]
+[assembly: AssemblyCopyright("Copyright © VAlmaev 2014")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fc9881ce-f57b-44c6-ab45-06e10d9a1396")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
